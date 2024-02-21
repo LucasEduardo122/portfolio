c2ae45da46474318a29dfb247c475536ce6f0c44
lucaseduardo122 <luklucas42@gmail.com>
2024-02-21 14:20:16 -0300

    Add logo, add metatags and add image cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,23 @@ import Footer from "@/components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "lucaseduardo.xyz",
+  title: "cloudlector.xyz",
   description: "Sou programador back-end e gosto de criar projetos incríveis. Estou sempre procurando expandir meus conhecimentos e adquirir novas habilidades para tornar meu trabalho mais eficiente.",
+  authors: [{ name: "Lucas Eduardo", url: "https://cloudlector.xyz" }],
+  icons: [
+    {
+      rel: "icon",
+      url: "/logo.png",
+      type: "image/png",
+      sizes: "256x256"
+    }
+  ],
+  robots: { index: true, follow: true },
+  creator: "Lucas Eduardo",
+  referrer: "origin-when-cross-origin",
+  publisher: "Lucas Eduardo",
+  twitter: { card: "summary_large_image", creator: "Lucas Eduardo", title: "cloudlector.xyz", images: "https://portfolio-lucaseduardo122.vercel.app/static/cover.png", description: "Sou programador back-end e gosto de criar projetos incríveis. Estou sempre procurando expandir meus conhecimentos e adquirir novas habilidades para tornar meu trabalho mais eficiente." },
+  openGraph: { title: "cloudlector.xyz", description: "Sou programador back-end e gosto de criar projetos incríveis. Estou sempre procurando expandir meus conhecimentos e adquirir novas habilidades para tornar meu trabalho mais eficiente.", images: "https://portfolio-lucaseduardo122.vercel.app/static/cover.png" }
 };
 
 export default function RootLayout({
