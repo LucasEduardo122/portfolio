8e37e71145960027ff52b054237d7f29e8eab2e6
Lucas Eduardo <luklucas42@gmail.com>
2024-06-22 20:37:40 -0300

    fix: Mudando html lang para 'pt-BR'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+    <html lang="pt-BR" className="dark" style={{ colorScheme: "dark" }}>
       <Head>
       <script
             dangerouslySetInnerHTML={{
